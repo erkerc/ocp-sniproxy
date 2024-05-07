515a252b44b6cef7f66cd118963fcecdfc7773b1
erkerc <erkan.ercan@gmail.com>
2024-05-07 17:41:24 +0300

    updated sni-install.sh

--- sni-install.sh
+++ sni-install.sh
@@ -1,9 +1,9 @@
 sudo -i
-#podman volume create sniproxy-api-conf
-#podman volume create sniproxy-apps-conf
 
-podman run -d --name sniproxy-apps -p 443:443/tcp  --security-opt label=disable -v ./volume-apps/:/data docker.io/atenart/sniproxy:latest --config=/data/sniproxy.conf
-podman run -d --name sniproxy-api -p 6443:6443/tcp  --security-opt label=disable -v ./volume-api/:/data docker.io/atenart/sniproxy:latest --config=/data/sniproxy.conf
+podman run -d --name sniproxy-api -p 6443:6443/tcp --security-opt label=disable -v /root/ocp-sniproxy/volume-api/:/data docker.io/atenart/sniproxy@sha256:7a24e4cb9aabfe76c353c08eeb069180ece801bec2bf90f5389d6acfe4257f4f -bind :6443 -conf /data/sniproxy.conf
+
+podman run -d --name sniproxy-apps -p 443:443/tcp --security-opt label=disable -v /root/ocp-sniproxy/volume-apps/:/data docker.io/atenart/sniproxy@sha256:7a24e4cb9aabfe76c353c08eeb069180ece801bec2bf90f5389d6acfe4257f4f --config=/data/sniproxy.conf
+
 
 podman generate systemd --new --name sniproxy-api -f
 podman generate systemd --new --name sniproxy-apps -f
@@ -16,15 +16,4 @@ systemctl enable container-sniproxy-apps.service
 systemctl start container-sniproxy-api.service
 systemctl start container-sniproxy-apps.service
 
-echo "api.$CLUSTER_NAME.$BASE_URL {
-  backend $API_IP:6443
-}" > volume-api/sniproxy.conf
-
-cat  volume-api/sniproxy.conf
-
-echo "apps.$CLUSTER_NAME.$BASE_URL{
-  backend $ROUTER_IP:443
-  http-redirect http,tls
-}" > volume-apps/sniproxy.conf
-
-cat volume-apps/sniproxy.conf
+ss -tlnp
